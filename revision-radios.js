9d9e887ce2cb06ea607aeb4a3035d41951f039f2
Rik de Boer <rikdeboer1@yahoo.com.au>
2011-06-28 14:44:36 +1000

    Improved Publication summary tab display when clicking node edit form radio buttons.

--- revision-radios.js
+++ revision-radios.js
@@ -7,16 +7,17 @@
       // Hide the "Published" check-box when "Create new revision and moderate" is pressed.
       var publishedBox = $('.form-item-status', context);
       if ($('.form-item-revision-operation .form-radio:checked').val() == 2) { // 2 == NEW_REVISION_WITH_MODERATION
+        $('#edit-status', context).val(0);
         publishedBox.hide();
       }
 
       $('.form-radio').click(function() {
         if ($('.form-item-revision-operation .form-radio:checked').val() == 2) {
-          publishedBox.hide();
           $('#edit-status', context).val(0);
-        //@todo: update the summary tab to reflect publication status
-        //var summaryText = $('fieldset.node-form-options', context).text();
-        //summaryText = summaryText.replace('Published', 'Not published');
+          publishedBox.hide();
+          var summaryTab = $('.vertical-tabs span.summary:last', context)
+          var updatedText = summaryTab.text().replace('Published', 'Not published');
+          summaryTab.text(updatedText);
         }
         else {
           publishedBox.show();
