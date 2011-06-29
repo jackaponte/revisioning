db390a0a3a5872b0aa659ba98f0f120863e0d59f
Rik de Boer <rikdeboer1@yahoo.com.au>
2011-06-29 10:11:39 +1000

    More complete fix for [#1201952]

--- revision-radios.js
+++ revision-radios.js
@@ -5,25 +5,33 @@
     attach: function (context) {
 
       // Hide the "Published" check-box when "Create new revision and moderate" is pressed.
-      var publishedBox = $('.form-item-status', context);
+      var publishedDiv = $('.form-item-status', context);
+
       if ($('.form-item-revision-operation .form-radio:checked').val() == 2) { // 2 == NEW_REVISION_WITH_MODERATION
-        $('#edit-status', context).val(0);
-        publishedBox.hide();
+        publishedDiv.hide(); // will already be unchecked, unless "Auto-publish" is on
       }
 
       $('.form-radio').click(function() {
-        if ($('.form-item-revision-operation .form-radio:checked').val() == 2) {
-          $('#edit-status', context).val(0);
-          publishedBox.hide();
-          var summaryTab = $('.vertical-tabs span.summary:last', context)
-          var updatedText = summaryTab.text().replace('Published', 'Not published');
+        clicked = $('.form-item-revision-operation .form-radio:checked').val();
+
+        if (clicked == 2) {
+          $('.form-item-status input', context).removeAttr('checked');
+          publishedDiv.hide();
+          // @todo: don't do this when "Auto-publish" is ticked on content type (but how?)
+          var summaryTab = $('.vertical-tabs span.summary:last', context);
+          var updatedText = summaryTab.text().replace(Drupal.t('Published'), Drupal.t('Not published'));
           summaryTab.text(updatedText);
         }
         else {
-          publishedBox.show();
+          publishedDiv.show();
+          if (clicked == 0) {
+            var summaryTab = $('.vertical-tabs span.summary:first', context)
+            var updatedText = summaryTab.text().replace(Drupal.t('New revision'), Drupal.t('No revision'));
+            summaryTab.text(updatedText);
+          }
         }
-      });
 
+      });
     }
   };
 
