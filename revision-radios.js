0ae9bc1128b663e4f5e11394bc2b41ebda677cc4
Rik de Boer <rikdeboer1@yahoo.com.au>
2011-07-09 16:51:09 +1000

    Better fix for [#1201952]

--- revision-radios.js
+++ revision-radios.js
@@ -4,31 +4,39 @@
   Drupal.behaviors.suppressPublishedForModeratedContent = {
     attach: function (context) {
 
-      // Hide the "Published" check-box when "Create new revision and moderate" is pressed.
       var publishedDiv = $('.form-item-status', context);
+      var firstSummaryTab = $('.vertical-tabs span.summary:first', context)
+      var lastSummaryTab  = $('.vertical-tabs span.summary:last', context);
 
-      if ($('.form-item-revision-operation .form-radio:checked').val() == 2) { // 2 == NEW_REVISION_WITH_MODERATION
-        publishedDiv.hide(); // will already be unchecked, unless "Auto-publish" is on
+      if ($('.form-item-revision-operation .form-radio:checked').val() == 2) { // NEW_REVISION_WITH_MODERATION
+        // Hide the "Published" check-box, as it does not apply in this mode
+        // because the new revision will always be unpublished. 
+        publishedDiv.hide();
+        updatedText = lastSummaryTab.text().replace(Drupal.t('Published'), Drupal.t('Not published'));
+        lastSummaryTab.text(updatedText);
       }
 
       $('.form-radio').click(function() {
         clicked = $('.form-item-revision-operation .form-radio:checked').val();
 
-        if (clicked == 2) {
-          $('.form-item-status input', context).removeAttr('checked');
+        if (clicked == 2) { // NEW_REVISION_WITH_MODERATION
           publishedDiv.hide();
-          // @todo: don't do this when "Auto-publish" is ticked on content type (but how?)
-          var summaryTab = $('.vertical-tabs span.summary:last', context);
-          var updatedText = summaryTab.text().replace(Drupal.t('Published'), Drupal.t('Not published'));
-          summaryTab.text(updatedText);
+          var updatedText = lastSummaryTab.text().replace(Drupal.t('Published'), Drupal.t('Not published'));
+          lastSummaryTab.text(updatedText);
         }
-        else {
+        else { // NO_REVISION or NEW_REVISION_NO_MODERATTION
           publishedDiv.show();
-          if (clicked == 0) {
-            var summaryTab = $('.vertical-tabs span.summary:first', context)
-            var updatedText = summaryTab.text().replace(Drupal.t('New revision'), Drupal.t('No revision'));
-            summaryTab.text(updatedText);
+          if (clicked == 0) { // NO_REVISION
+            var updatedText = firstSummaryTab.text().replace(Drupal.t('New revision'), Drupal.t('No revision'));
+            firstSummaryTab.text(updatedText);
           }
+          if ($('.form-item-status input').is(':checked')) {
+            updatedText = lastSummaryTab.text().replace(Drupal.t('Not published'), Drupal.t('Published'));
+          }
+          else {
+            updatedText = lastSummaryTab.text().replace(Drupal.t('Published'), Drupal.t('Not published'));
+          }
+          lastSummaryTab.text(updatedText);
         }
 
       });
@@ -36,3 +44,4 @@
   };
 
 })(jQuery);
+
