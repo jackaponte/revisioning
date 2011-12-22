9e500e7b42de2419f513584f902c782921993efb
Rik de Boer <rikdeboer1@yahoo.com.au>
2011-12-22 13:19:13 +1100

    Major overhaul of Revisioning Scheduler. Added permission control, javascript to control relationship between Published checkbox and publication date, config options.

--- revisioning_scheduler/revision-schedule.js
+++ revisioning_scheduler/revision-schedule.js
@@ -1,24 +1,41 @@
 
 (function ($) {
 
+  /* This reflect the logic of revisioning_scheduler_node_presave().
+   * Scheduling a publication date is only appropriate if:
+   * o the node is NOT published
+   * o the node is already published, but the new revision to be created goes in
+   *   moderation, i.e will not be published yet
+   */
   Drupal.behaviors.showPublicationDateTimeField = {
     attach: function (context) {
-      var publication_date = $('.form-item-publication-date');
-      var new_revision_in_moderation_radio = $('#edit-revision-operation-2');
+      var publishedBox = $('.form-item-status input');
+      var newRevisionInModerationRadio = $('#edit-revision-operation-2');
+      var publicationDate = $('.form-item-publication-date');
 
-      if (!new_revision_in_moderation_radio.is(':checked')) {
-        publication_date.hide();
+      // Page-load: hide the publication date textbox, if necessary
+      if ((publishedBox.is(':checked') && !newRevisionInModerationRadio.is(':checked'))) {
+        publicationDate.hide();
       }
-      $('#edit-revision-operation input').click(function() {
-        if (new_revision_in_moderation_radio.is(':checked')) {
-          publication_date.show();
+
+      // Define handlers for clicks on the Published box and moderation radio buttons.
+      publishedBox.click(function() {
+        if (!publishedBox.is(':checked') || newRevisionInModerationRadio.is(':checked')) {
+          publicationDate.show();
         }
         else {
-          publication_date.hide();
+          publicationDate.hide();
+        }
+      });
+      $('#edit-revision-operation input').click(function() {
+        if (!publishedBox.is(':checked') || newRevisionInModerationRadio.is(':checked')) {
+          publicationDate.show();
+        }
+        else {
+          publicationDate.hide();
         }
       });
     }
   };
 
 })(jQuery);
-
